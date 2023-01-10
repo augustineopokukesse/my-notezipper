6e00e7ae7b2d6c4c56f72840b2a7405f8b9aa704
augustineopokukesse <aokesse@gmail.com>
2023-01-10 23:07:06 +0000

    Add header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Header />
+      <main style={{minHeight: "93vh"}}>
+        
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Navbar, Nav, NavDropdown, Container, Form, Button } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar bg="primary" expand="lg" variant='dark'>
+      <Container>
+        <Navbar.Brand href="/">Note Zipper</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="m-auto"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Form inline>
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+            </Form>
+          </Nav>  
+           
+          
+          <Nav> 
+            <Nav.Link href="#action1">My Notes</Nav.Link>
+            <NavDropdown title="Augustine Opoku-Kesse" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action4">
+                My profile
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+            
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
